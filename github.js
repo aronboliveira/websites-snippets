b3c55d9ac5a9fc0d13985bc918ef3495048795fd
Aron Barbosa de Oliveira <aron.b.96@gmail.com>
2025-03-27 12:01:50 -0300

    Update github.js

--- github.js
+++ github.js
@@ -1,33 +1,56 @@
 const scrolling = { behavior: 'smooth', block: 'center',  inline: 'center' };
-/* Profile click */
-const ca = document.querySelector('.avatar.circle');
-ca.click();
-/* Settings click */
-setTimeout(() => window.open(document.querySelector('a[href="/settings/profile"]')?.href, '_self'), 2000);
-/* Developer settings click */
-setTimeout(() => window.open(document.querySelector('a[href="/settings/apps"]')?.href, '_self'), 2000);
-/* Personal access tokens click */
-setTimeout(() => window.open(document.querySelector('a[href="/settings/tokens"]')?.href, '_self'), 2000);
-/* Details for generating new token */
-setTimeout(() => document.querySelector('.details-reset.details-overlay.d-inline-block.position-relative')?.click(), 2000);
-/* Generate new token */
-setTimeout(() => window.open(document.querySelector('a[href="/settings/tokens/new"]')?.href, '_self'), 2000);
-/* Fillings */
-setTimeout(() => {
-  const nt = document.getElementById('oauth_access_description');
-  if (nt && 'value' in nt) {
-    nt.scrollIntoView(scrolling);
-    nt.value = 'Access to private repos';
-  }
+/** 
+*  Profile click 
+*  @return {void}
+**/
+const openSettings() => {
+  document.querySelector('.avatar.circle')?..click();
+  setTimeout(() => window.open(document.querySelector('a[href="/settings/profile"]')?.href, '_self'), 2000);
+}
+/** 
+*  Developer settings click 
+*  @return {void}
+**/
+const openDevSettings() => {
+  setTimeout(() => window.open(document.querySelector('a[href="/settings/apps"]')?.href, '_self'), 2000);
+}
+/** 
+*  Personal access tokens click
+*  @return {void}
+**/
+const openTokens() => {
+  setTimeout(() => window.open(document.querySelector('a[href="/settings/tokens"]')?.href, '_self'), 2000);
+}
+/** 
+*  Generate new classic token 
+*  @return {void}
+**/
+const openNewToken() => {
+  /* Details for generating new token */
+  setTimeout(() => document.querySelector('.details-reset.details-overlay.d-inline-block.position-relative')?.click(), 2000);
+  /* Generate new token */
+  setTimeout(() => window.open(document.querySelector('a[href="/settings/tokens/new"]')?.href, '_self'), 2000);
+}
+/** 
+*  Fillings
+*  @return {void}
+**/
+const fillForm() => {
   setTimeout(() => {
-    const rp = document.getElementsByName('oauth_access[scopes][]')[0];
-    if (rp && 'checked' in rp) {
-      rp.scrollIntoView(scrolling);
-      if (!rp.checked) {
-        rp.dispatchEvent(new MouseEvent('click', { bubbles: false }));
-        rp.checked = true;
-      }
+    const nt = document.getElementById('oauth_access_description');
+    if (nt && 'value' in nt) {
+      nt.scrollIntoView(scrolling);
+      nt.value = 'Access to private repos';
     }
-  }, 500);
-}, 2000);
-
+    setTimeout(() => {
+      const rp = document.getElementsByName('oauth_access[scopes][]')[0];
+      if (rp && 'checked' in rp) {
+        rp.scrollIntoView(scrolling);
+        if (!rp.checked) {
+          rp.dispatchEvent(new MouseEvent('click', { bubbles: false }));
+          rp.checked = true;
+        }
+      }
+    }, 500);
+  }, 2000);
+}
