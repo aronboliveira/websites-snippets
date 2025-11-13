eb610b55f3b2f2e2bbd3fa6b1181f7277a787ea6
Aron Barbosa de Oliveira <aron.b.96@gmail.com>
2025-11-13 14:18:19 -0300

    Refactor range input extraction bookmarklet
    Updated the JavaScript bookmarklet to streamline the extraction of range input values from a Canva editor panel.

--- extract_canva_active_ranges.js
+++ extract_canva_active_ranges.js
@@ -1,28 +1,4 @@
-javascript: (() => {
-  const anchor = document.createElement("a");
-  anchor.href = URL.createObjectURL(
-    new Blob([
-      Array.from(document.querySelectorAll("input[type='range']")).reduce(
-        (acc, e) => {
-          return (
-            acc +
-            `Name: ${e.labels[0]?.innerText ?? "#NOLABEL"}; Value: ${
-              e.value
-            }    \n`
-          );
-        },
-        ""
-      ),
-    ]),
-    { type: "text/plain" }
-  );
-  anchor.download = "range-input-values-canva-" + Date.now() + ".txt";
-  document.body.appendChild(anchor);
-  anchor.click();
-  document.body.removeChild(anchor);
-  URL.revokeObjectURL(anchor.href);
-/*
-Simple snippet to extract and save the range values on screen from a Canva editor panel.
-Save it as a bookmark and click on it to trigger.
+javascript:(()=>{const content=Array.from(document.querySelectorAll("input[type='range']")).reduce((acc,e)=>{return acc+`Name: ${e.labels[0]?.innerText??"#NOLABEL"}; Value: ${e.value}\n`;},"");const anchor=document.createElement("a");anchor.href=URL.createObjectURL(new Blob([content],{type:"text/plain"}));anchor.download="range-input-values-canva-"+Date.now()+".txt";document.body.appendChild(anchor);anchor.click();document.body.removeChild(anchor);URL.revokeObjectURL(anchor.href);})();
+/* REMOVE EVERYTHING HERE AND BELOW AFTER DOWNLOADING OR COPYING 
+Simple snippet to copy the range input values from a Canva editor panel into a downloadable .txt
 */
-})();
